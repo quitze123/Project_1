08c8e75f4125836c406ed7ba0d66db6102de7df9
Jose Flores-Martinez <git@github.com:quitze123/lab_2_CSC349.git>
2018-09-21 21:13:46 -0700

    Working arp/ethernet fuctions

--- Project_1/trace.c
+++ Project_1/trace.c
@@ -8,17 +8,23 @@
 #include <netinet/in.h>
 #include <netinet/ether.h>
 
+#define ETHERNET 14
 #define DST_MAC 6
 #define SRC_MAC 6
 #define SENDER_MAC 6
 #define SENDER_IP 4
+#define TARGET_MAC 6
+#define TARGET_IP 4
 #define ARP 0x0806
+#define REQUEST 0x0001
+#define REPLY 0x0002
 
 struct ethernet_header
 {
    uint8_t dst_addr[DST_MAC];
    uint8_t src_addr[SRC_MAC];
    uint16_t type;
+
 } __attribute__((packed));
 
 struct arp_header
@@ -28,8 +34,10 @@ struct arp_header
    uint8_t hardware_size;
    uint8_t protocol_size;
    uint16_t opcode;
-   uint8_t sndr_mac_addr[SENDER_MAC];
-   uint8_t sndr_ip_addr[SENDER_IP];
+   uint8_t sndr_mac[SENDER_MAC];
+   uint8_t sndr_ip[SENDER_IP];
+   uint8_t trgt_mac[TARGET_MAC];
+   uint8_t trgt_ip[TARGET_IP];
 
 } __attribute__((packed));
 
@@ -53,6 +61,7 @@ pcap_t * open_file(char * f_name)
 
 void run(pcap_t * pcap_ptr);
 uint16_t ethernet(const u_char * pkt_data);
+void arp(const u_char * pkt_data);
 
 int main(int argc, char ** argv)
 {
@@ -88,7 +97,7 @@ void run(pcap_t * pcap_ptr)
       if(type == ARP)
       {
          printf("\t\tType: ARP\n");
-
+         arp(pkt_data);
       }
    }
 }
@@ -102,4 +111,30 @@ uint16_t ethernet(const u_char * pkt_data)
    printf("\t\tSource MAC: %s\n", ether_ntoa((struct ether_addr *)eh->src_addr));
    
    return ntohs(eh->type);
+}
+
+void arp(const u_char * pkt_data)
+{
+   struct arp_header * ah = (struct arp_header *)(pkt_data 
+      + sizeof(struct ethernet_header));
+   
+    
+   printf("\n\tARP Header\n");
+   printf("\t\tOpcode: ");
+   if(ntohs(ah->opcode) == REPLY)
+   {
+      printf("Reply\n");
+   }
+   else if(ntohs(ah->opcode) == REQUEST)
+   {
+       printf("Request\n");
+   }
+   printf("\t\tSender MAC: %s\n", 
+      ether_ntoa((struct ether_addr *)ah->sndr_mac));
+   printf("\t\tSender IP: %s\n", 
+      inet_ntoa(*((struct in_addr *)(ah->sndr_ip))));
+   printf("\t\tTarget MAC: %s\n", 
+      ether_ntoa((struct ether_addr *)ah->trgt_mac));
+   printf("\t\tTarget IP: %s\n\n", 
+      inet_ntoa(*((struct in_addr *)(ah->trgt_ip))));
 }
